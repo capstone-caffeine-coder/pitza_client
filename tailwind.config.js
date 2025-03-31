3d6d43fc2274bb92b8ede048146ae6452e40bf08
Minhyung Cho <dup33333@hufs.ac.kr>
2025-03-31 22:22:17 +0900

    chore : add necessary dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        pitza: {
-          lightPink: "#F29595", // 밝은 핑크
-          softPink: "#FECCCC", // 부드러운 핑크
-          vividRed: "#E51A1A", // 선명한 빨강
-          pastelPink: "#F8BFBF", // 파스텔 핑크
-          coral: "#FF7F7F", // 코랄
-          deepRed: "#C21818", // 딥 레드
-          blush: "#F4A3A3", // 블러쉬 핑크
-        },
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        destructive: "var(--color-destructive)",
+        accent: "var(--color-accent)",
+        background: "var(--color-background)",
+        foreground: "var(--color-foreground)",
+        lightRose: "var(--color-lightRose)",
+        softPeach: "var(--color-softPeach)",
+        warmCoral: "var(--color-warmCoral)",
+        cherryBlossom: "var(--color-cherryBlossom)",
+        sunsetRed: "var(--color-sunsetRed)",
+        deepCrimson: "var(--color-deepCrimson)",
+        mutedRose: "var(--color-mutedRose)",
       },
     },
   },
