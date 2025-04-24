438e8af1a9b58bf182fd5197b146b934134171b0
Minhyung Cho <dup33333@hufs.ac.kr>
2025-04-24 22:00:59 +0900

    chore : tsconfig 및 vite.config 수정하여 kakao.maps 타입 추가 및 경로 설정 개선

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./"),
+      },
+    ],
+  },
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
