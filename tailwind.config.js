e81454f84e44e186052b1ce6ec464fcfcec0fb76
Minhyung Cho <dup33333@hufs.ac.kr>
2025-04-24 22:00:40 +0900

    style : tailwind 위로 올라오면서 밝아지는 애니메이션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,21 @@ export default {
         deepCrimson: "var(--color-deepCrimson)",
         mutedRose: "var(--color-mutedRose)",
       },
+      keyframes: {
+        "fade-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-up": "fade-up 0.5s ease-out forwards",
+      },
     },
   },
   plugins: [],
