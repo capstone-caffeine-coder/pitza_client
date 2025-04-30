2edeac7cc4d0e15f9b8e7fb967fd19553980096f
Minhyung Cho <dup33333@hufs.ac.kr>
2025-04-30 19:15:44 +0900

    feat: 채팅 라우트에 인증 가드 추가

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -1,9 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import authGuard from "@/src/components/wrapper/AuthGuard";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/chat')({
-  component: RouteComponent,
-})
+const ProtectedChat = authGuard(RouteComponent);
+export const Route = createFileRoute("/chat")({
+  component: ProtectedChat,
+});
 
 function RouteComponent() {
-  return <div>Hello "/chat"!</div>
+  return <div>Hello "/chat"!</div>;
 }
