c95ee9464532efb319c6186a00118882d145f772
Minhyung Cho <dup33333@hufs.ac.kr>
2025-04-30 19:16:30 +0900

    feat: createRootRouteWithContext로 라우트 초기화 및 주변 헌혈원 컴포넌트 추가

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import {
-  createRootRoute,
+  createRootRouteWithContext,
   Link,
   Outlet,
   ValidateToPath,
@@ -8,8 +8,10 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { AiOutlineHome } from "react-icons/ai";
 import { MdOutlineBloodtype } from "react-icons/md";
 import { BiDonateBlood } from "react-icons/bi";
-import { CgProfile } from "react-icons/cg";
 import { BsChatLeftDots } from "react-icons/bs";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { AuthState } from "@/src/store/authStore";
+import { initializeApp } from "firebase/app";
 
 const navRoutes: {
   to: ValidateToPath;
@@ -17,19 +19,19 @@ const navRoutes: {
   icon: React.ReactNode;
 }[] = [
   {
-    to: "/",
-    title: "홈",
-    icon: <AiOutlineHome />,
-  },
-  {
-    to: "/donercard",
+    to: "/bloodcard",
     title: "헌혈증",
     icon: <MdOutlineBloodtype />,
   },
   {
-    to: "/donate",
+    to: "/donation",
     title: "지정헌혈",
     icon: <BiDonateBlood />,
+  },
+  {
+    to: "/",
+    title: "홈",
+    icon: <AiOutlineHome />,
   },
   {
     to: "/chat",
@@ -37,29 +39,48 @@ const navRoutes: {
     icon: <BsChatLeftDots />,
   },
   {
-    to: "/mypage",
-    title: "마이",
-    icon: <CgProfile />,
+    to: "/center",
+    title: "주변 헌혈원",
+    icon: <FaMapLocationDot />,
   },
 ];
 
-export const Route = createRootRoute({
-  component: () => (
+interface RouterContext {
+  auth: AuthState;
+}
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+initializeApp(firebaseConfig);
+
+const RootRoute = () => {
+  return (
     <main className="relative mx-auto h-screen max-h-screen max-w-screen-sm overflow-y-auto pb-20">
-      <div className="bg-primary absolute bottom-0 flex h-20 w-full">
+      <div className="absolute bottom-0 flex h-20 w-full bg-primary">
         {navRoutes.map((route) => (
           <Link
             key={route.to}
             to={route.to}
-            className="flex flex-1 flex-col items-center justify-center text-center text-white hover:bg-pitza-softPink/80 [&_svg]:h-6 [&_svg]:w-6 [&_svg]:text-white"
+            className="hover:bg-pitza-softPink/80 flex flex-1 flex-col items-center justify-center text-center text-white [&_svg]:h-6 [&_svg]:w-6 [&_svg]:text-white"
           >
             {route.icon}
             {route.title}
           </Link>
         ))}
       </div>
-      <Outlet />
+      <div className="h-full overflow-y-auto">
+        <Outlet />
+      </div>
       <TanStackRouterDevtools />
     </main>
-  ),
+  );
+};
+export const Route = createRootRouteWithContext<RouterContext>()({
+  component: RootRoute,
 });
