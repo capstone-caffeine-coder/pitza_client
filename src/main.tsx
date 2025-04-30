3d6214614039b8cd6611e26b94be1236ee7a8ffc
Minhyung Cho <dup33333@hufs.ac.kr>
2025-04-30 19:11:53 +0900

    feat : MSW 초기화 로직 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./index.css";
-
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
@@ -13,12 +13,24 @@ declare module "@tanstack/react-router" {
   }
 }
 
+if (import.meta.env.DEV) {
+  import("./mocks/browser")
+    .then(({ worker }) => {
+      worker.start();
+    })
+    .catch((error) => {
+      console.error("Failed to start MSW", error);
+    });
+}
+const queryClient = new QueryClient();
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
