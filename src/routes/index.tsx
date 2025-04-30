384ec232881eda2411243d957648883564bdabf2
Minhyung Cho <dup33333@hufs.ac.kr>
2025-04-30 19:16:11 +0900

    feat: 헌혈 매칭 페이지 레이아웃 및 컴포넌트 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,7 @@
+import { Box } from "@/src/components/common/box";
+import { Button } from "@/src/components/common/button";
+import RecentBloodCard from "@/src/domains/BloodCard/components/recentBloodCard";
+import BloodHoldStatus from "@/src/domains/BloodHoldStatus/components";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -6,8 +10,19 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-4">
-      <h3>Welcome Home!</h3>
+    <div className="flex flex-col gap-4 p-4">
+      <Box withIcon>
+        <div>
+          <p>
+            헌혈할 때가 되셨네요!
+            <br />
+            지정헌혈 해줄 사람을 찾아볼까요?
+          </p>
+          <Button className="mt-1 w-full">지정헌혈 매칭하기</Button>
+        </div>
+      </Box>
+      <BloodHoldStatus />
+      <RecentBloodCard />
     </div>
   );
 }
