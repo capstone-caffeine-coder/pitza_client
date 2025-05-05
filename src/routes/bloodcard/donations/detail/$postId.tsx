59128a8bb1e4168a4329cf5d3f58abfd15bbab3a
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-05 20:22:15 +0900

    feat: 헌혈증 기부 상세 페이지 라우트 및 컴포넌트 추가

---
+++ src/routes/bloodcard/donations/detail/$postId.tsx
@@ -0,0 +1,41 @@
+import { ErrorComponent } from "@/src/components/common/error";
+import Header from "@/src/components/common/header";
+import { SpinnerModal } from "@/src/components/common/spinner";
+import { getBloodCardDonateDetail } from "@/src/domains/BloodCard/api";
+import { Info } from "@/src/domains/BloodCard/components/info";
+import { createFileRoute, getRouteApi, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/bloodcard/donations/detail/$postId")({
+  loader: async ({ params: { postId } }) => getBloodCardDonateDetail(postId),
+  pendingComponent: SpinnerModal,
+  component: RouteComponent,
+  errorComponent: ErrorComponent,
+  onError: () => {
+    throw redirect({ to: "/" });
+  },
+});
+
+function RouteComponent() {
+  const routeApi = getRouteApi("/bloodcard/donations/detail/$postId");
+  const data = routeApi.useLoaderData();
+  return (
+    <>
+      <Header title="헌혈증 기부 상세" />
+      <div className="flex flex-col gap-4 p-4">
+        <img
+          src={data.image}
+          className="h-[300px] w-full rounded-xl object-cover shadow-xl"
+        />
+        <section>
+          <p className="py-2 text-lg">{data.nickname}</p>
+          <hr />
+          <Info field="지역" value={data.donationLocation} />
+        </section>
+        <section>
+          <h2 className="text-xl">소개</h2>
+          <p className="p-2">{data.introduce}</p>
+        </section>
+      </div>
+    </>
+  );
+}
