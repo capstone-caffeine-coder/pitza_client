7cf9e39528408603adbfcf139ea3a1d8dcf5d4b4
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-05 20:20:07 +0900

    feat: LocationProvider에서 focusOnMyLocation 기능 추가 및 LocationContext 값 업데이트

--- src/domains/BloodDonationCenter/components/LocationProvider.tsx
+++ src/domains/BloodDonationCenter/components/LocationProvider.tsx
@@ -1,15 +1,23 @@
-import { LocationContext } from "@/src/domains/BloodDonationCenter/store/locationContext";
+import { LocationContext } from "@/src/domains/BloodDonationCenter/hooks/useLocationContext";
 import useGeoLocation from "@/src/hooks/useGeoLocation";
 
 function LocationProvider({ children }: { children: React.ReactNode }) {
-  const { location, setLocation } = useGeoLocation();
+  const { location, setLocation, focusOnMyLocation } = useGeoLocation();
 
-  const changeCenterLocation = (latitude: number, longitude: number) => {
+  const changeCenterLocation = ({
+    latitude,
+    longitude,
+  }: {
+    latitude: number;
+    longitude: number;
+  }) => {
     setLocation({ latitude, longitude });
   };
 
   return (
-    <LocationContext.Provider value={{ location, changeCenterLocation }}>
+    <LocationContext.Provider
+      value={{ location, changeCenterLocation, focusOnMyLocation }}
+    >
       {children}
     </LocationContext.Provider>
   );
