0a02ebb1c1e00a26f67c5fad51df641c8ef13412
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-05 20:22:08 +0900

    feat: DonationCenterProvider로 CenterMap 및 DonationCenterList 감싸기

--- src/routes/center.tsx
+++ src/routes/center.tsx
@@ -1,6 +1,7 @@
 import Header from "@/src/components/common/header";
 import { CenterMap } from "@/src/domains/BloodDonationCenter/components/CenterMap";
 import DonationCenterList from "@/src/domains/BloodDonationCenter/components/DonationCenterList";
+import { DonationCenterProvider } from "@/src/domains/BloodDonationCenter/components/DonationCenterProvider";
 import LocationProvider from "@/src/domains/BloodDonationCenter/components/LocationProvider";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -13,8 +14,10 @@ function RouteComponent() {
     <main>
       <Header title="주변 헌혈원 찾기" />
       <LocationProvider>
-        <CenterMap />
-        <DonationCenterList />
+        <DonationCenterProvider>
+          <CenterMap />
+          <DonationCenterList />
+        </DonationCenterProvider>
       </LocationProvider>
     </main>
   );
