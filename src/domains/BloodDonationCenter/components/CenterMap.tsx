e529f70e09742040f19427cd0e53e8bde34263f7
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-05 20:19:45 +0900

    feat: CenterMap 컴포넌트에서 헌혈 센터 마커 추가 및 현재 위치 버튼 수정

--- src/domains/BloodDonationCenter/components/CenterMap.tsx
+++ src/domains/BloodDonationCenter/components/CenterMap.tsx
@@ -1,13 +1,15 @@
-import { useLocationContext } from "@/src/domains/BloodDonationCenter/store/locationContext";
+import { useDonationCenter } from "@/src/domains/BloodDonationCenter/hooks/useDonationCenter";
+import { useLocationContext } from "@/src/domains/BloodDonationCenter/hooks/useLocationContext";
 import useKakaoLoader from "@/src/hooks/useKakaoLoader";
 import React from "react";
 import { FaLocationCrosshairs } from "react-icons/fa6";
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 export function CenterMap() {
   useKakaoLoader();
   const mapRef = React.useRef<kakao.maps.Map>(null);
-  const { location } = useLocationContext();
+  const { location, focusOnMyLocation } = useLocationContext();
+  const { centers } = useDonationCenter();
 
   const handleCenterToCurrentLocation = async () => {
     setTimeout(() => {
@@ -32,11 +34,19 @@ export function CenterMap() {
         isPanto={true}
         level={3}
         className="h-full w-full"
-      />
-
+      >
+        {centers?.map((center) => (
+          <MapMarker
+            position={{
+              lat: center.location.latitude,
+              lng: center.location.longitude,
+            }}
+          />
+        ))}
+      </Map>
       <div
         className="absolute bottom-4 right-4 z-50 cursor-pointer rounded-full bg-white p-2 shadow hover:bg-pink-200"
-        onClick={handleCenterToCurrentLocation}
+        onClick={focusOnMyLocation}
       >
         <FaLocationCrosshairs size={20} />
       </div>
