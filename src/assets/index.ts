0bf53f97636b3696c6921de973bf9b0e1eb559b2
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-06 21:48:51 +0900

    chore : pitza_spinner asset 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,16 @@
-const assets = ["characterIcon", "kakao_login", 404, "error"] as const;
+const assets = [
+  "characterIcon",
+  "kakao_login",
+  404,
+  "error",
+  "pitza_spinner",
+] as const;
 
 type Assets = (typeof assets)[number];
 const ASSET_PREFIX = "/src/assets";
 const assetMap: Record<Assets, string> = {
   characterIcon: `${ASSET_PREFIX}/pitza.png`,
+  pitza_spinner: `${ASSET_PREFIX}/pitza_spinner.png`,
   kakao_login: `${ASSET_PREFIX}/kakao_login.png`,
   404: `${ASSET_PREFIX}/404.png`,
   error: `${ASSET_PREFIX}/error.png`,
