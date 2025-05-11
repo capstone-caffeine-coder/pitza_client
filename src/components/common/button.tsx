b5143b38d584f7f44c0f3e3007094c132927926c
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-12 00:30:35 +0900

    fix: 버튼 컴포넌트의 destructive 변형 스타일 수정 및 none 변형 추가

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -8,14 +8,14 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-white",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        destructive: "bg-mutedRose text-white hover:bg-destructive/90 ",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        none: "",
       },
       size: {
         default: "h-10 px-4 py-2",
