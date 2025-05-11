dae029aba972347be53a42ac34b428ba1777f774
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-12 00:30:51 +0900

    feat: Modal 컴포넌트에 최대 높이 및 최소 높이 설정 추가

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -6,7 +6,10 @@ const Modal = ({
 }: { children: React.ReactNode } & HtmlHTMLAttributes<HTMLDivElement>) => {
   return (
     <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-black/50">
-      <div className="w-[90%]" {...props}>
+      <div
+        className="max-h-[80vh] min-h-32 w-[90%] overflow-y-auto rounded-xl bg-white"
+        {...props}
+      >
         {children}
       </div>
     </div>
