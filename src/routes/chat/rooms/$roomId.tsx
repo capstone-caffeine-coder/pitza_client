a4c69822b9d135814df3c338230c47b0939264d7
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-12 00:31:53 +0900

    feat: 채팅 헤더에 신고하기 버튼 추가 및 신고 모달 기능 구현

--- src/routes/chat/rooms/$roomId.tsx
+++ src/routes/chat/rooms/$roomId.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/src/components/common/button";
 import { ErrorComponent } from "@/src/components/common/error";
 import Header from "@/src/components/common/header";
 import { SpinnerModal } from "@/src/components/common/spinner";
@@ -5,7 +6,10 @@ import { getChatLogs } from "@/src/domains/Chat/api";
 import ChatInput from "@/src/domains/Chat/components/chatInput";
 import ChatLogs from "@/src/domains/Chat/components/chatLogs";
 import { ChatProvider } from "@/src/domains/Chat/components/chatProvider";
+import ReportModal from "@/src/domains/Chat/components/reportModal";
 import { createFileRoute, getRouteApi } from "@tanstack/react-router";
+import { useState } from "react";
+import { GoReport } from "react-icons/go";
 
 export const Route = createFileRoute("/chat/rooms/$roomId")({
   loader: async ({ params: { roomId } }) => getChatLogs(roomId),
@@ -17,17 +21,31 @@ export const Route = createFileRoute("/chat/rooms/$roomId")({
 function RouteComponent() {
   const routeApi = getRouteApi("/chat/rooms/$roomId");
   const { messages, room_id } = routeApi.useLoaderData();
+  const [modal, setModal] = useState(false);
 
   return (
-    <div className="relative h-full w-full">
-      <Header
-        title="채팅"
-        className="absolute left-0 top-0 w-full bg-white p-4 text-center"
-      />
-      <ChatProvider roomId={room_id}>
-        <ChatLogs messages={messages} />
-        <ChatInput />
-      </ChatProvider>
-    </div>
+    <>
+      <div className="relative h-full w-full">
+        <Header
+          title="채팅"
+          className="absolute left-0 top-0 w-full bg-white p-4 text-center"
+        >
+          <div className="absolute right-0 top-0 flex h-full items-center p-4">
+            <Button
+              variant={"none"}
+              type="button"
+              onClick={() => setModal(true)}
+            >
+              <GoReport size={20} />
+            </Button>
+          </div>
+        </Header>
+        <ChatProvider roomId={room_id}>
+          <ChatLogs messages={messages} />
+          <ChatInput />
+        </ChatProvider>
+      </div>
+      <ReportModal visible={modal} toggle={() => setModal((prev) => !prev)} />
+    </>
   );
 }
