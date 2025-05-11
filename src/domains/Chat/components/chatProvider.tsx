86c3ece8926e7d004e8e8a384839e2cc8c94db49
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-12 00:31:13 +0900

    feat: 채팅 프로바이더에 이미지 메시지 수신 및 전송 기능 추가

--- src/domains/Chat/components/chatProvider.tsx
+++ src/domains/Chat/components/chatProvider.tsx
@@ -1,27 +1,31 @@
 // context/SocketContext.tsx
 import { SocketContext } from "@/src/domains/Chat/hooks/useChatContext";
-import { Message } from "@/src/domains/Chat/types";
+import { Message, ImageMessage } from "@/src/domains/Chat/types";
 import React, { useEffect, useReducer } from "react";
 import { io, Socket } from "socket.io-client";
 
 interface SocketState {
   socket: Socket | null;
   isConnected: boolean;
-  messages: Message[];
+  messages: (Message | ImageMessage)[];
+  chatroomId: string;
 }
 
 type Action =
   | { type: "CHATLOGS"; payload: Message[] }
-  | { type: "CONNECT" }
+  | { type: "CONNECT"; payload: { chatroomId: string } }
   | { type: "DISCONNECT" }
   | { type: "RECEIVE_MESSAGE"; payload: Message }
-  | { type: "SEND_MESSAGE"; payload: Message };
+  | { type: "RECEIVE_IMAGE"; payload: ImageMessage }
+  | { type: "SEND_MESSAGE"; payload: Omit<Message, "chatroom_id"> }
+  | { type: "SEND_IMAGE"; payload: Omit<ImageMessage, "chatroom_id"> };
 
 // 초기 상태
 const initialState: SocketState = {
   socket: null,
   isConnected: false,
   messages: [],
+  chatroomId: "",
 };
 
 // Reducer 정의
@@ -29,14 +33,33 @@ const socketReducer = (state: SocketState, action: Action): SocketState => {
   switch (action.type) {
     case "CONNECT":
       console.log("Socket connected");
-      return { ...state, isConnected: true };
+      return {
+        ...state,
+        isConnected: true,
+        chatroomId: action.payload.chatroomId,
+      };
     case "DISCONNECT":
       return { ...state, isConnected: false, socket: null };
     case "RECEIVE_MESSAGE":
-      console.log("qwqwr", state.messages);
+      return { ...state, messages: [...state.messages, action.payload] };
+    case "RECEIVE_IMAGE":
       return { ...state, messages: [...state.messages, action.payload] };
     case "SEND_MESSAGE":
-      return { ...state, messages: [...state.messages, action.payload] };
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          { chatroom_id: state.chatroomId, ...action.payload },
+        ],
+      };
+    case "SEND_IMAGE":
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          { chatroom_id: state.chatroomId, ...action.payload },
+        ],
+      };
     default:
       return state;
   }
@@ -60,23 +83,31 @@ export const ChatProvider = ({
 
     socket.on("connect", () => {
       console.log("Socket connected");
-      dispatch({ type: "CONNECT" });
+      dispatch({ type: "CONNECT", payload: { chatroomId: roomId } });
     });
 
     socket.on("disconnect", () => {
       dispatch({ type: "DISCONNECT" });
     });
 
-    socket.on("message", (msg: Message) => {
-      console.log("Received message:", msg);
-      dispatch({ type: "RECEIVE_MESSAGE", payload: msg });
+    socket.on("message", (msg: Omit<Message, "chatroom_id">) => {
+      dispatch({
+        type: "RECEIVE_MESSAGE",
+        payload: { ...msg, chatroom_id: state.chatroomId },
+      });
     });
 
+    socket.on("image", (msg: Omit<ImageMessage, "chatroom_id">) => {
+      dispatch({
+        type: "RECEIVE_IMAGE",
+        payload: { ...msg, chatroom_id: state.chatroomId },
+      });
+    });
     return () => {
       socket.emit("close");
       socket.disconnect();
     };
-  }, []);
+  }, [roomId, state.chatroomId]);
 
   return (
     <SocketContext.Provider value={{ state, dispatch }}>
