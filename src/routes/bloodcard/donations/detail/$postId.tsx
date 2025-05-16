e0dfd0944396bf59833a0ee1490963b304b0491a
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-16 11:59:20 +0900

    feat: 헌혈증 기부 상세 페이지에 채팅방 생성 기능 추가 및 로딩 스피너 구현

--- src/routes/bloodcard/donations/detail/$postId.tsx
+++ src/routes/bloodcard/donations/detail/$postId.tsx
@@ -1,9 +1,17 @@
+import { createChatRoom } from "@/src/api/chat";
+import { Button } from "@/src/components/common/button";
 import { ErrorComponent } from "@/src/components/common/error";
 import Header from "@/src/components/common/header";
 import { SpinnerModal } from "@/src/components/common/spinner";
 import { getBloodCardDonateDetail } from "@/src/domains/BloodCard/api";
 import { Info } from "@/src/domains/BloodCard/components/info";
-import { createFileRoute, getRouteApi, redirect } from "@tanstack/react-router";
+import { useMutation } from "@tanstack/react-query";
+import {
+  createFileRoute,
+  getRouteApi,
+  redirect,
+  useRouter,
+} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/bloodcard/donations/detail/$postId")({
   loader: async ({ params: { postId } }) => getBloodCardDonateDetail(postId),
@@ -18,6 +26,16 @@ export const Route = createFileRoute("/bloodcard/donations/detail/$postId")({
 function RouteComponent() {
   const routeApi = getRouteApi("/bloodcard/donations/detail/$postId");
   const data = routeApi.useLoaderData();
+  const { navigate } = useRouter();
+  const { mutate, isPending } = useMutation({
+    mutationFn: createChatRoom,
+    onSuccess: (data) => {
+      navigate({
+        to: "/chat/rooms/$roomId",
+        params: { roomId: data.chatroom_id },
+      });
+    },
+  });
   return (
     <>
       <Header title="헌혈증 기부 상세" />
@@ -35,7 +53,14 @@ function RouteComponent() {
           <h2 className="text-xl">소개</h2>
           <p className="p-2">{data.introduce}</p>
         </section>
+        <Button
+          type="button"
+          onClick={() => mutate({ post_id: data.id, receiver_id: "qwrqwr" })}
+        >
+          연락하기
+        </Button>
       </div>
+      {isPending && <SpinnerModal />}
     </>
   );
 }
