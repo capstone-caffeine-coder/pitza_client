d7933573d8080e8d9679df7d8f92a93f5b28226a
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-22 22:33:28 +0900

    feat: apiServerInstance 추가 및 401 에러 처리 로직 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
+import { useAuthStore } from "@/src/store/authStore";
 import axios from "axios";
 
 const apiInstance = axios.create({
@@ -9,4 +10,25 @@ const apiInstance = axios.create({
   timeout: 5000,
 });
 
-export { apiInstance };
+const apiServerInstance = axios.create({
+  baseURL: "http://localhost:8000/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+  timeout: 5000,
+});
+
+apiServerInstance.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      useAuthStore.getState().logout();
+    }
+    return Promise.reject(error);
+  },
+);
+
+export { apiInstance, apiServerInstance };
