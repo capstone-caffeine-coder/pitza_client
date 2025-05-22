e4e2da39be569e3fae14c8741ad8f550638e1d7c
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-23 01:23:09 +0900

    feat: 헌혈 등록 폼에 등록번호 필드 추가 및 UI 개선

--- src/routes/donation/create.tsx
+++ src/routes/donation/create.tsx
@@ -29,6 +29,7 @@ function RouteComponent() {
       deadline: new Date(),
       story: "",
       image: undefined,
+      doncationId: "",
     },
   });
   const { isPending, mutate: createRequest } = useMutation({
@@ -46,134 +47,147 @@ function RouteComponent() {
   };
 
   return (
-    <main className="p-4">
+    <>
       <Header title="지정헌혈 등록하기" />
-      <Box withIcon>
-        지정헌혈을 등록하고
-        <br />
-        사용자에게 헌혈을 요청해보세요.
-      </Box>
+      <main className="p-4">
+        <Box withIcon>
+          지정헌혈을 등록하고
+          <br />
+          사용자에게 헌혈을 요청해보세요.
+        </Box>
 
-      <div className="flex w-full animate-fade-up items-center justify-center">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="mt-6 flex w-full max-w-md flex-col gap-4"
-        >
-          <FileInput
-            onChange={(e) => {
-              const file = (e.target as HTMLInputElement).files?.[0];
-              if (file) {
-                setValue("image", file);
-              }
-            }}
+        <div className="w-full animate-fade-up items-center justify-center">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="min-w-md mt-6 flex w-full flex-col gap-4"
           >
-            {donateImage ? (
-              <img
-                src={URL.createObjectURL(donateImage)}
-                alt="헌혈증 이미지"
-                className="flex h-full w-full rounded-xl object-cover"
-              />
-            ) : (
-              <div className="flex h-full w-full items-center justify-center rounded-xl bg-primary">
-                <div className="flex flex-col items-center justify-center gap-2">
-                  <IoMdCamera size={30} className="text-white" />
-                  <p className="text-white">이미지를 업로드 해주세요</p>
+            <FileInput
+              onChange={(e) => {
+                const file = (e.target as HTMLInputElement).files?.[0];
+                if (file) {
+                  setValue("image", file);
+                }
+              }}
+            >
+              {donateImage ? (
+                <img
+                  src={URL.createObjectURL(donateImage)}
+                  alt="헌혈증 이미지"
+                  className="flex h-full w-full rounded-xl object-cover"
+                />
+              ) : (
+                <div className="flex h-full w-full items-center justify-center rounded-xl bg-primary">
+                  <div className="flex flex-col items-center justify-center gap-2">
+                    <IoMdCamera size={30} className="text-white" />
+                    <p className="text-white">이미지를 업로드 해주세요</p>
+                  </div>
                 </div>
-              </div>
-            )}
-          </FileInput>
-          {/* 혈액형 */}
-          <FormRow label="혈액형">
-            <Select
-              className="w-1/2 rounded-full bg-primary text-center text-white"
-              options={[...BLOOOD_TYPES]}
-              placeholder="혈액형을 선택하세요"
-              value={watch("bloodType")}
-              {...register("bloodType")}
-            />
-          </FormRow>
+              )}
+            </FileInput>
+            {/* 혈액형 */}
+            <FormRow label="혈액형">
+              <Select
+                className="w-1/2 rounded-full bg-primary text-center text-white"
+                options={[...BLOOOD_TYPES]}
+                placeholder="혈액형을 선택하세요"
+                value={watch("bloodType")}
+                {...register("bloodType")}
+              />
+            </FormRow>
 
-          {/* 나이 */}
-          <FormRow label="나이">
-            <input
-              type="number"
-              className="w-1/2 rounded-full bg-primary px-3 py-2 text-center text-white"
-              max={100}
-              min={0}
-              {...register("age", { valueAsNumber: true })}
-            />
-          </FormRow>
+            {/* 나이 */}
+            <FormRow label="나이">
+              <input
+                type="number"
+                className="w-1/2 rounded-full bg-primary px-3 py-2 text-center text-white"
+                max={100}
+                min={0}
+                {...register("age", { valueAsNumber: true })}
+              />
+            </FormRow>
 
-          {/* 성별 */}
-          <FormRow label="성별">
-            <div className="flex gap-6 pl-4">
-              <label className="flex items-center gap-2">
-                <input
-                  type="radio"
-                  value="FEMALE"
-                  {...register("gender")}
-                  className="h-4 w-4 appearance-none rounded border border-gray-400 checked:border-transparent checked:bg-primary"
-                />
-                여성
-              </label>
-              <label className="flex items-center gap-2">
-                <input
-                  type="radio"
-                  value="MALE"
-                  {...register("gender")}
-                  className="h-4 w-4 appearance-none rounded border border-gray-400 checked:border-transparent checked:bg-primary"
-                />
-                남성
-              </label>
-            </div>
-          </FormRow>
-
-          {/* 지역 */}
-          <FormRow label="지역">
-            <div className="grid w-full grid-cols-3 gap-2">
-              {REGIONS.map((region) => (
-                <label key={region}>
+            {/* 성별 */}
+            <FormRow label="성별">
+              <div className="flex gap-6 pl-4">
+                <label className="flex items-center gap-2">
                   <input
                     type="radio"
-                    value={region}
-                    {...register("location")}
-                    className="peer hidden"
+                    value="FEMALE"
+                    {...register("gender")}
+                    className="h-4 w-4 appearance-none rounded border border-gray-400 checked:border-transparent checked:bg-primary"
                   />
-                  <div className="cursor-pointer rounded border py-1 text-center peer-checked:bg-primary peer-checked:text-white">
-                    {region}
-                  </div>
+                  여성
                 </label>
-              ))}
-            </div>
-          </FormRow>
+                <label className="flex items-center gap-2">
+                  <input
+                    type="radio"
+                    value="MALE"
+                    {...register("gender")}
+                    className="h-4 w-4 appearance-none rounded border border-gray-400 checked:border-transparent checked:bg-primary"
+                  />
+                  남성
+                </label>
+              </div>
+            </FormRow>
 
-          {/* 수혈 필요 날짜 */}
-          <FormRow label="수혈 필요 날짜">
-            <input
-              type="date"
-              className="w-1/2 rounded-full border bg-primary px-4 py-2 text-white"
-              {...register("deadline", { valueAsDate: true })}
-            />
-          </FormRow>
-          <FormRow label="사연">
-            <textarea
-              className="w-full resize-none overflow-hidden rounded-xl border-2 border-primary p-2"
-              rows={1}
-              onInput={(e) => {
-                const target = e.target as HTMLTextAreaElement;
-                target.style.height = "auto";
-                target.style.height = `${target.scrollHeight}px`;
-              }}
-              {...register("story")}
-            />
-          </FormRow>
+            {/* 지역 */}
+            <FormRow label="지역">
+              <div className="grid w-full grid-cols-3 gap-2">
+                {REGIONS.map((region) => (
+                  <label key={region}>
+                    <input
+                      type="radio"
+                      value={region}
+                      {...register("location")}
+                      className="peer hidden"
+                    />
+                    <div className="cursor-pointer rounded border py-1 text-center peer-checked:bg-primary peer-checked:text-white">
+                      {region}
+                    </div>
+                  </label>
+                ))}
+              </div>
+            </FormRow>
 
-          <Button type="submit" variant={"default"}>
-            등록하기
-          </Button>
-        </form>
-      </div>
-      {isPending && <SpinnerModal />}
-    </main>
+            {/* 수혈 필요 날짜 */}
+            <FormRow label="수혈 필요 날짜">
+              <input
+                type="date"
+                className="w-1/2 rounded-full border bg-primary px-4 py-2 text-white"
+                {...register("deadline", { valueAsDate: true })}
+              />
+            </FormRow>
+            <FormRow label="사연">
+              <textarea
+                className="w-full resize-none overflow-hidden rounded-xl border-2 border-primary p-2"
+                rows={1}
+                onInput={(e) => {
+                  const target = e.target as HTMLTextAreaElement;
+                  target.style.height = "auto";
+                  target.style.height = `${target.scrollHeight}px`;
+                }}
+                {...register("story")}
+              />
+            </FormRow>
+            <FormRow label="등록번호">
+              <input
+                className="w-3/4 resize-none overflow-hidden rounded-xl border-2 border-primary p-2"
+                onInput={(e) => {
+                  const target = e.target as HTMLTextAreaElement;
+                  target.style.height = "auto";
+                  target.style.height = `${target.scrollHeight}px`;
+                }}
+                {...register("doncationId")}
+              />
+            </FormRow>
+
+            <Button type="submit" variant={"default"}>
+              등록하기
+            </Button>
+          </form>
+        </div>
+        {isPending && <SpinnerModal />}
+      </main>
+    </>
   );
 }
