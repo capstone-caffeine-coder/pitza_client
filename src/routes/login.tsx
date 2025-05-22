22ce3d2e69c48cd2a12e3b63463aaa410c31968f
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-23 01:22:59 +0900

    feat: 카카오 로그인 기능 추가 및 UI 개선

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,5 +1,6 @@
 import { assetMap } from "@/src/assets";
 import { createFileRoute } from "@tanstack/react-router";
+import { useRef } from "react";
 
 export const Route = createFileRoute("/login")({
   component: RouteComponent,
@@ -16,16 +17,32 @@ function RouteComponent() {
   // const onSubmit = (data: LoginForm) => {
   //   console.log("로그인 시도:", data);
   // };
+  const kakaoLoginRef = useRef<HTMLAnchorElement>(null);
+  function handleKakaoLogin() {
+    if (kakaoLoginRef.current) {
+      kakaoLoginRef.current.click();
+    }
+  }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-4">
+    <div className="flex h-full flex-col items-center justify-center p-4">
       <img
         src={assetMap["characterIcon"]}
         alt="핏자 로고"
-        className="animate-fade-up mb-4 h-56 w-56"
+        className="mb-4 w-56 animate-fade-up"
       />
       <h1 className="mb-4 text-2xl font-bold">핏자 로그인</h1>
-      <img src={assetMap["kakao_login"]} alt="카카오 로그인" className="h-14" />
+      <img
+        src={assetMap["kakao_login"]}
+        alt="카카오 로그인"
+        className="h-14 cursor-pointer"
+        onClick={handleKakaoLogin}
+      />
+      <a
+        href="http://localhost:8000/login/kakao"
+        className="hidden"
+        ref={kakaoLoginRef}
+      ></a>
       {/* <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex w-full max-w-sm flex-col gap-4"
