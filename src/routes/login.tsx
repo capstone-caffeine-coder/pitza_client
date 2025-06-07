af6b5064b647a6cc42d0c85a1c32a9496a297951
Minhyung Cho <dup33333@hufs.ac.kr>
2025-06-07 19:14:12 +0900

    feat : 구글 로그인 추가

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -6,17 +6,7 @@ export const Route = createFileRoute("/login")({
   component: RouteComponent,
 });
 
-// type LoginForm = {
-//   email: string;
-//   password: string;
-// };
-
 function RouteComponent() {
-  // const { register, handleSubmit } = useForm<LoginForm>();
-
-  // const onSubmit = (data: LoginForm) => {
-  //   console.log("로그인 시도:", data);
-  // };
   const kakaoLoginRef = useRef<HTMLAnchorElement>(null);
   function handleKakaoLogin() {
     if (kakaoLoginRef.current) {
@@ -24,8 +14,15 @@ function RouteComponent() {
     }
   }
 
+  const googleLoginRef = useRef<HTMLAnchorElement>(null);
+  function handleGoogleLogin() {
+    if (googleLoginRef.current) {
+      googleLoginRef.current.click();
+    }
+  }
+
   return (
-    <div className="flex h-full flex-col items-center justify-center p-4">
+    <div className="flex h-full flex-col items-center justify-center gap-5 p-4">
       <img
         src={assetMap["characterIcon"]}
         alt="핏자 로고"
@@ -35,7 +32,7 @@ function RouteComponent() {
       <img
         src={assetMap["kakao_login"]}
         alt="카카오 로그인"
-        className="h-14 cursor-pointer"
+        className="h-[45px] cursor-pointer"
         onClick={handleKakaoLogin}
       />
       <a
@@ -43,29 +40,17 @@ function RouteComponent() {
         className="hidden"
         ref={kakaoLoginRef}
       ></a>
-      {/* <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex w-full max-w-sm flex-col gap-4"
-      >
-        <input
-          type="email"
-          placeholder="이메일"
-          {...register("email", { required: true })}
-          className="rounded border p-2"
-        />
-        <input
-          type="password"
-          placeholder="비밀번호"
-          {...register("password", { required: true })}
-          className="rounded border p-2"
-        />
-        <Button
-          type="submit"
-          className="rounded p-2 text-white hover:bg-blue-600"
-        >
-          로그인
-        </Button>
-      </form> */}
+      <img
+        src={"/src/assets/google.svg"}
+        alt="구글 로그인"
+        className="h-[45px] w-[300px] cursor-pointer"
+        onClick={handleGoogleLogin}
+      />
+      <a
+        href="http://localhost:8000/login/google"
+        className="hidden"
+        ref={googleLoginRef}
+      ></a>
     </div>
   );
 }
